f5d685721e9d7b0c024320b26296e3d179baba57
Jonathan Moussa <jemoussa@vt.edu>
2023-09-26 03:33:46 -0400

    Given CMake info on git hash of MOPAC repo

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,7 @@ cmake -Bbuild -GNinja \
   ${CMAKE_ARGS} \
   -DCMAKE_BUILD_TYPE=Release \
   -DBLA_VENDOR=Generic \
+  -DGIT_HASH=ad2d88444242f2314cb93037bc14d9a75193be13 \
   -DTESTS=OFF
 
 cmake --build build
