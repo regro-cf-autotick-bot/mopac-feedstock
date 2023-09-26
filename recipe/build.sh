dcb810fa829dd9b171e7273be66817e8c4bd99cc
Jonathan Moussa <jemoussa@vt.edu>
2023-09-26 04:53:46 -0400

    Switch from python3 path to hint

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,7 @@ cmake -Bbuild -GNinja \
   -DCMAKE_BUILD_TYPE=Release \
   -DBLA_VENDOR=Generic \
   -DGIT_HASH=ad2d88444242f2314cb93037bc14d9a75193be13 \
-  -DPython3_EXECUTABLE="$PYTHON"
+  -DPython3_ROOT_DIR="$CONDA_PREFIX"
 
 cmake --build build
 cmake --install build
