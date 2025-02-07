7936c14fd55982c022be447c42d91ed90e5cb733
Jonathan E. Moussa <40802025+godotalgorithm@users.noreply.github.com>
2025-02-07 13:32:28 -0500

    Update bld.bat to use C malloc/free and export Windows symbols

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,11 @@
-cmake -Bbuild -GNinja -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DCMAKE_BUILD_TYPE=Release -DGIT_HASH=OFF -DTESTS=OFF
+cmake -Bbuild ^
+  -GNinja ^
+  -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DGIT_HASH=OFF ^
+  -DUSE_C_MALLOC=ON ^
+  -DFORCE_EXPORT_SYMBOLS=ON ^
+  -DTESTS=OFF
 if errorlevel 1 exit 1
 cmake --build build
 if errorlevel 1 exit 1
